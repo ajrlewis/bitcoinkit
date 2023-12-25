8e05697ba89d2a295d35df1be0e342e329003f4a
ajrlewis <git@ajrlewis.com>
2023-12-25 11:41:47 +0000

    Added to and from millisatoshis methods.

--- bitcoinkit/bitcoin_units.py
+++ bitcoinkit/bitcoin_units.py
@@ -1,4 +1,5 @@
 SATOSHIS_PER_BITCOIN = 100_000_000
+MILLI_SATOSHIS_PER_SATOSHI = 1_000
 
 
 def bitcoins_to_satoshis(bitcoins):
@@ -9,6 +10,14 @@ def satoshis_to_bitcoins(satoshis):
     return satoshis / SATOSHIS_PER_BITCOIN
 
 
+def satoshis_to_millisatoshis(satoshis):
+    return satoshis * MILLI_SATOSHIS_PER_SATOSHI
+
+
+def millisatoshis_to_satoshis(millisatoshis):
+    return millisatoshis / MILLI_SATOSHIS_PER_SATOSHI
+
+
 def main():
     bitcoins = 0.5
     satoshis = bitcoins_to_satoshis(bitcoins)
@@ -17,6 +26,14 @@ def main():
     satoshis = 50_000_000
     bitcoins = satoshis_to_bitcoins(satoshis)
     print(f"{satoshis:,} satoshis is equal to {bitcoins} bitcoins")
+
+    satoshis = 1_000_000
+    millisatoshis = satoshis_to_millisatoshis(satoshis)
+    print(f"{satoshis:,} satoshis is equal to {millisatoshis:,} millisatoshis")
+
+    millisatoshis = 1_000_000
+    satoshis = millisatoshis_to_satoshis(millisatoshis)
+    print(f"{millisatoshis:,} millisatoshis is equal to {satoshis} satoshis")
 
 
 if __name__ == "__main__":
